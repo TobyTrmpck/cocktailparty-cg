f705da00fc28cf97f61d40162053ff67bdc05e0d
TobiasTrempeck <tobias.trempeck@capgemini.com>
2021-08-30 18:07:37 +0200

    created maps for cocktail party

--- OL_Innenraum_HGSS_Newtiteuf.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="OL_Innenraum_HGSS_Newtiteuf" tilewidth="32" tileheight="32" tilecount="496" columns="16">
- <image source="tilesets/OL_Innenraum_HGSS_Newtiteuf.png" width="533" height="1002"/>
-</tileset>
